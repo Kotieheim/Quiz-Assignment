42d6cce20de25ec11eeeda24f47b57c7fc5bbd52
Dakota Heim <kotieheim@gmail.com>
2019-07-10 09:43:34 -0600

    Layout
    finish layout

---
+++ index.js
@@ -0,0 +1,70 @@
+let questionNumber = 0;
+let score = 0;
+
+// first step - make button to begin quiz, then hide everything
+// after clicked, generate first question.
+
+
+function hideQuiz() {
+
+}
+
+//button click to start quiz
+function startQuiz() {
+
+}
+
+//makes the question in HTML
+function makeQuestion() {
+
+}
+
+//makes the actual question in the quiz DOM
+function renderQuestion() {
+
+}
+
+//answer selected and submitted, decide if right or wrong
+function answerSelection() {
+
+}
+
+//display correct, update score
+function correctAnswer() {
+
+}
+
+//display wrong, try again button maybe?
+function wrongAnswer() {
+
+}
+
+
+
+//use changeScore to increment the score
+function updateScore() {
+
+}
+//question completed, click next to generate new question
+function nextQuestion() {
+
+}
+
+
+
+//adds to the question counter
+function changeQuestionNumber() {
+
+}
+
+//adds to the score counter
+function changeScore() {
+
+}
+
+function createQuiz() {
+    hideQuiz()
+    startQuiz()
+
+}
+
